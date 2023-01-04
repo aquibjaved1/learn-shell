229cad2a02ea297eb6fe05abd1cff847954ec81c
Aquib Javed <aquib.javed1234@gmail.com>
2023-01-05 01:59:25 +0400

    date

---
+++ 03-variable.sh
@@ -0,0 +1,11 @@
+## If we assign a name to a set of data is called as variable.
+
+name=Aquib
+class=Devops
+
+echo student name - $student
+echo class name - $(class)
+
+DATE=2023-01-03
+DATE=$(date +%F)
+echo Hey, Today date is $DATE
