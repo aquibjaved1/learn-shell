4a52adc17f7c177cc1034d9e53f0460e3854b9a1
Aquib Javed <aquib.javed1234@gmail.com>
2023-01-05 01:44:33 +0400

    color code

--- 02.print.sh
+++ 02.print.sh
@@ -3,12 +3,12 @@ echo Hello world
 echo "**** Hello world ****"
 
 #Color code Syntax : echo -e "\e[COLmMESSAGE\{0m"
-# Red       -31 - echo -e "\e[31mMESSAGE\{0m"
-# Green     -32 - echo -e "\e[32mMESSAGE\{0m"
-# Yellow    -33 - echo -e "\e[33mMESSAGE\{0m"
-# Blue      -34 - echo -e "\e[34mMESSAGE\{0m"
-# Magenta   -35 - echo -e "\e[35mMESSAGE\{0m"
-# Cyan      -36 - echo -e "\e[36mMESSAGE\{0m"
+# Red       -31 - echo -e "\e[31mMESSAGE\e{0m"
+# Green     -32 - echo -e "\e[32mMESSAGE\e{0m"
+# Yellow    -33 - echo -e "\e[33mMESSAGE\e{0m"
+# Blue      -34 - echo -e "\e[34mMESSAGE\e{0m"
+# Magenta   -35 - echo -e "\e[35mMESSAGE\e{0m"
+# Cyan      -36 - echo -e "\e[36mMESSAGE\e{0m"
 
  echo -e "\e[31mMESSAGE\e{0m"
  echo -e "\e[32mMESSAGE\e{0m"
