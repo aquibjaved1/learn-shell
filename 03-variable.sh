73a6ec42f8086606a18ee0905bb024bfc1a7e314
Aquib Javed <aquib.javed1234@gmail.com>
2023-01-05 02:05:43 +0400

    date add

--- 03-variable.sh
+++ 03-variable.sh
@@ -3,9 +3,12 @@
 name=Aquib
 class=Devops
 
-echo student name - $student
+echo student name - $name
 echo class name - ${class}
 
 DATE=2023-01-03
 DATE=$(date +%F)
 echo Hey, Today date is $DATE
+
+ADD=$((2+3))
+echo ADD - $ADD
