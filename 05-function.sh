e0730efff4d1d63b7071459ed414144039276ff0
Aquib Javed <aquib.javed1234@gmail.com>
2023-01-12 01:08:13 +0400

    function

---
+++ 05-function.sh
@@ -0,0 +1,11 @@
+#Functions are like variables.
+# Variable - when we assign a name to a set of data then it is variable.
+# Function - when we assign a name to a set of commands then it is function.
+
+# Function Declare
+example() {
+  echo I am a example function
+}
+
+##Call a function
+example
